7c640894325c7e7f38ad0c61a903d69fa47afc2d
Joachim <etu44137@henallux.be>
2023-05-01 13:35:12 +0200

    initial commit

---
+++ src/main/java/joachim/lejeune/peditrack/PeditrackApplication.java
@@ -0,0 +1,13 @@
+package joachim.lejeune.peditrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeditrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeditrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joachim/lejeune/peditrack/PeditrackApplicationTests.java
@@ -0,0 +1,13 @@
+package joachim.lejeune.peditrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeditrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
