f0d1ab1729fd70a5ad2208266ccf8a4d463d69dc
JLEJ <Joachim.Lejeune@pierret.net>
2025-09-30 13:04:03 +0200

    ajout de test pour UserController

--- src/integration-test/java/joachim.lejeune.peditrack/controller/user/UserControllerIT.java
+++ src/integration-test/java/joachim.lejeune.peditrack/controller/user/UserControllerIT.java
@@ -1,21 +1,29 @@
 package joachim.lejeune.peditrack.controller.user;
 
+import joachim.lejeune.peditrack.bodyDto.PatientBodyDto;
 import joachim.lejeune.peditrack.bodyDto.UserBodyDto;
 import joachim.lejeune.peditrack.controller.ApplicationControllerIT;
+import joachim.lejeune.peditrack.dto.UserDto;
+import joachim.lejeune.peditrack.model.role.Role;
+import joachim.lejeune.peditrack.model.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SqlConfig(encoding = "UTF-8")
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {
@@ -33,6 +41,32 @@ public class UserControllerIT extends ApplicationControllerIT {
     }
 
     @Test
+    void getUsers_shouldReturnListOfUsers() throws Exception {
+
+        mockMvc.perform(get("/api/users/all"))
+                .andExpect(status().isAccepted())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].name", is("admin")));
+    }
+
+    @Test
+    void registerUser_shouldReturnOkWhenUserCreated() throws Exception {
+
+        UserBodyDto userBodyDto = getUserBodyDto();
+
+        // Convertir en JSON
+        String userJson = objectMapper.writeValueAsString(userBodyDto);
+
+        mockMvc.perform(post("/api/users/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(content().string("User registered successfully: joachim"));
+    }
+
+    @Test
     void userAlreadyExist() throws Exception {
         this.mockMvc.perform(get("/api/users/exist")
                         .contentType("application/json")
@@ -41,5 +75,11 @@ public class UserControllerIT extends ApplicationControllerIT {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", is(true)))
         ;
+    }
+
+    private UserBodyDto getUserBodyDto() {
+        UserBodyDto userBodyDto= new UserBodyDto("machin","abcdefgh", "machin@truc.be");
+        userBodyDto.setRoles(List.of(Role.USER.name()));
+        return userBodyDto;
     }
 }
