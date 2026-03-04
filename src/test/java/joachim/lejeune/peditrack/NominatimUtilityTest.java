cf709f22dba52498537900857321553cf7764501
LJKhnor <135867702+LJKhnor@users.noreply.github.com>
2026-03-04 21:00:58 +0100

    test nominatim a terminer

---
+++ src/test/java/joachim/lejeune/peditrack/NominatimUtilityTest.java
@@ -0,0 +1,24 @@
+package joachim.lejeune.peditrack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NominatimUtilityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addressConverter() throws IOException {
+        Point2D point2D = NominatimUtility.AddressConverter("chaussée de marche 250 5100 jambes");
+        assertNotNull(point2D.getX());
+        assertNotNull(point2D.getY());
+    }
+}
---
+++ src/test/java/joachim/lejeune/peditrack/model/role/RolesJsonConverterTest.java
@@ -0,0 +1,26 @@
+package joachim.lejeune.peditrack.model.role;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RolesJsonConverterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void convertToDatabaseColumn() {
+    }
+
+    @Test
+    void convertToEntityAttribute() {
+    }
+}
