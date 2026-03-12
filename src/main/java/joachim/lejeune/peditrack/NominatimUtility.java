0edb5a767caf33db8e1d9e57edbdaa8aa587d2c7
LJKhnor <135867702+LJKhnor@users.noreply.github.com>
2026-03-12 12:24:05 +0100

    fix NPE AddressConverter

--- src/main/java/joachim/lejeune/peditrack/NominatimUtility.java
+++ src/main/java/joachim/lejeune/peditrack/NominatimUtility.java
@@ -1,9 +1,12 @@
 package joachim.lejeune.peditrack;
 
 
+import joachim.lejeune.peditrack.controller.auth.AuthTokenFilter;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.awt.geom.Point2D;
@@ -17,22 +20,27 @@ import java.net.URLEncoder;
 @Component
 public class NominatimUtility {
 
+    private static final Logger LOG = LoggerFactory.getLogger(NominatimUtility.class);
 
     public static Point2D AddressConverter(String street) throws IOException {
+        LOG.info("Enter method AddressConverter");
 
-        String endpointUrl = "https://nominatim.openstreetmap.org/search?q=" + URLEncoder.encode(street, "UTF-8") + "&format=json&addressdetails=1";
         Point2D point = new Point2D.Float();
+        if(street != null){
+            String endpointUrl = "https://nominatim.openstreetmap.org/search?q=" + URLEncoder.encode(street, "UTF-8") + "&format=json&addressdetails=1";
 
-        JSONArray results = getResultAddressList(endpointUrl);
-        double lat = 0.0;
-        double lon = 0.0;
-        if (results.length() > 0){
-            JSONObject firstResult = results.getJSONObject(0);
+            JSONArray results = getResultAddressList(endpointUrl);
+            double lat = 0.0;
+            double lon = 0.0;
+            if (!results.isEmpty()){
+                JSONObject firstResult = results.getJSONObject(0);
 
-            lat = firstResult.getDouble("lat");
-            lon = firstResult.getDouble("lon");
+                lat = firstResult.getDouble("lat");
+                lon = firstResult.getDouble("lon");
+            }
+            point.setLocation(lat, lon);
         }
-        point.setLocation(lat, lon);
+
         return point;
     }
 
